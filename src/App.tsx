be78114d3fe4961d11dc1a9b3949f3aabfa25666
Saraswatsahoo <ssahoo.code.13@gmail.com>
2025-09-10 20:38:20 +0530

    Bottom Bar Toggle Button Changing Model on clicking the toggle button 10/9/25

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
 import { Canvas } from "@react-three/fiber"
 import "./App.css"
 import Room from "./component/Room";
+import ToggleButtons from "./component/ToggleButtons";
+import BottomBar from "./component/BottomBar";
+import { useEffect, useState } from "react";
+
 
 export default function App(){
 
+  const [ roomNumber, setRoomNumber ] = useState(1);
+  const [ room, setRoom ] = useState("Living Room")
+
+  useEffect(()=>{
+    if(roomNumber == 1) setRoom("Living Room")
+    else if(roomNumber == 2) setRoom("Bedroom")
+    else if(roomNumber == 3) setRoom("Kitchen")
+  },[roomNumber])  
+
   return(
-    <div className=" flex flex-col items-center justify-center h-screen bg-black">
+    <div className=" flex flex-col items-center h-screen justify-center bg-black">
+      <div className=" h-full w-full mt-[20px]">
         <Canvas>
           <directionalLight position={[1, 2, 3]} intensity={1.5}/>
           <ambientLight intensity={1.5}/>
-          <Room scale={1.5} position={[0, 0, 0]}/>
+          <Room roomNumber={roomNumber} scale={1.2} position={[0, 0, 0]} rotation={[0, Math.PI / 2, 0]}/>
         </Canvas>
+      </div>
+      <div className=" mt-[30px]">
+        <ToggleButtons room={room} roomNumber={roomNumber} setRoomNumber={setRoomNumber}/>
+      </div>
+      <div className="w-[40%] m-[30px]">
+        <BottomBar />
+      </div>
     </div>
-      
   )
 }
--- src/component/Room.tsx
+++ src/component/Room.tsx
@@ -1,12 +1,21 @@
 import { OrbitControls, useGLTF } from "@react-three/drei"
+import { useEffect, useState } from "react";
 
-export default function Room({ scale, position }: { scale?: number, position?: [number, number, number] }) {
+export default function Room({ scale, position, rotation, roomNumber }: { scale?: number, position?: [number, number, number], rotation?: [number, number, number], roomNumber: number }) {
 
-    const { scene } = useGLTF("/models/room.glb");
+    const [ roomScene, setRoomScene ] = useState("/models/livingroom.glb");
+
+    useEffect(()=>{
+        if(roomNumber == 1) setRoomScene("./models/livingroom.glb");
+        else if(roomNumber == 2) setRoomScene("./models/bedroom.glb");
+        else if(roomNumber == 3) setRoomScene("./models/kitchen.glb");
+    },[roomNumber]);
+
+    const { scene } = useGLTF(roomScene);
 
     return(
         <>
-            <primitive object={scene} scale={scale} position={position} />
+            <primitive object={scene} scale={scale} position={position} rotation={rotation} onPointerDown={(e:Event)=>{console.log(e)}}/>
             <OrbitControls enableZoom={true} />
         </>
     )
